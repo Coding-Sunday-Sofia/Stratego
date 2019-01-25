2302b30d4257100b88844a22932b5050b9aa1e9f
Boyana Kantarska <boianaradkova@gmail.com>
2019-01-25 14:58:58 +0200

    Project reorganization.

---
+++ src/edu/asu/stratego/game/ClientSocket.java
@@ -0,0 +1,104 @@
+package edu.asu.stratego.game;
+
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+
+import edu.asu.stratego.gui.ConnectionScene;
+
+/**
+ * Client socket that connects to the server. The client should only use one
+ * socket to connect to the server.
+ */
+public final class ClientSocket {
+
+	private static BufferedReader stdin;
+	private static FileWriter writer;
+	private static ArrayList<String> list;
+
+	private static Socket socket = null;
+
+	/**
+	 * Prevents an instance of this class from being instantiated.
+	 */
+	private ClientSocket() {
+		/* Intentionally Empty */ }
+
+	/**
+	 * Attempts a connection to the server and updates saved server ips file
+	 * 
+	 * @param serverIP server IP address
+	 * @param port     server port number
+	 */
+	public static void connect(String serverIP, int port) throws UnknownHostException, IOException {
+		socket = new Socket(serverIP, port); // attempt connection
+
+		// take inputs from saved server file
+		stdin = new BufferedReader(new InputStreamReader(new FileInputStream(ConnectionScene.serverFile)));
+		writer = new FileWriter(ConnectionScene.serverFile, true);
+		list = new ArrayList<String>();
+		String input = stdin.readLine();
+		while (input != null) {
+			list.add(input);
+			input = stdin.readLine();
+		}
+		stdin.close();
+
+		// if ip was valid and not already present in save file, write ip to save file
+		if (ConnectionScene.isValid(serverIP) && !list.contains(encrypt(serverIP))) {
+			try {
+				writer.write(encrypt(serverIP) + System.getProperty("line.separator"));
+				writer.flush();
+				writer.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public static void reconnect() {
+		try {
+			socket = new Socket(ClientFileManager.getLastGameIp(), ClientFileManager.getLastPort());
+
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * encrypts server ip to prevent easy tampering
+	 *
+	 * @return encrypted value of a server ip
+	 */
+	public static String encrypt(String entered) {
+		String result = "";
+
+		char[] values = entered.toCharArray();
+
+		int[] encrypted = new int[values.length];
+
+		for (int i = 0; i < values.length; i++) {
+			encrypted[i] = (int) Math.pow((int) values[i], 2);
+
+			result += Integer.toString(encrypted[i]) + "$";
+		}
+
+		return ConnectionScene.reverse(result);
+	}
+
+	/**
+	 * Returns the one and only instance of the client socket.
+	 * 
+	 * @return the Socket used to establish a connection between the client and the
+	 *         server. The socket may be null.
+	 */
+	public static Socket getInstance() {
+		return socket;
+	}
+}
---
+++ src/edu/asu/stratego/game/Move.java
@@ -0,0 +1,124 @@
+package edu.asu.stratego.game;
+
+import java.awt.Point;
+import java.io.Serializable;
+
+public class Move implements Serializable {
+
+	private static final long serialVersionUID = -8315478849105334331L;
+
+	private Point start = new Point(-1, -1);
+	private Point end = new Point(-1, -1);
+
+	private PieceColor moveColor = null;
+
+	private Piece startPiece;
+	private Piece endPiece;
+
+	private boolean isAttack;
+	private boolean attackWin;
+	private boolean defendWin;
+
+	private boolean isRedConnected = true;
+	private boolean isBlueConnected = true;
+
+	public boolean isRedConnected() {
+		return isRedConnected;
+	}
+
+	public boolean isBlueConnected() {
+		return isBlueConnected;
+	}
+
+	public void setRedConnection(boolean connectionStatus) {
+		isRedConnected = connectionStatus;
+	}
+
+	public void setBlueConnection(boolean connectionStatus) {
+		isBlueConnected = connectionStatus;
+	}
+
+	public boolean isAttackMove() {
+		return isAttack;
+	}
+
+	public void setAttackMove(boolean bool) {
+		isAttack = bool;
+	}
+
+	public boolean isPieceSelected() {
+		return (start.x != -1 && start.y != -1);
+	}
+
+	public int getRowStart() {
+		return start.x;
+	}
+
+	public int getColStart() {
+		return start.y;
+	}
+
+	public Point getStart() {
+		return start;
+	}
+
+	public Point getEnd() {
+		return end;
+	}
+
+	public void setStart(int rowStart, int colStart) {
+		start = new Point(rowStart, colStart);
+	}
+
+	public void setEnd(int rowEnd, int colEnd) {
+		end = new Point(rowEnd, colEnd);
+	}
+
+	public void setStart(Point Start) {
+		start = Start;
+	}
+
+	public void setEnd(Point End) {
+		end = End;
+	}
+
+	public PieceColor getMoveColor() {
+		return moveColor;
+	}
+
+	public void setMoveColor(PieceColor moveColor) {
+		this.moveColor = moveColor;
+	}
+
+	public Piece getStartPiece() {
+		return startPiece;
+	}
+
+	public void setStartPiece(Piece startPiece) {
+		this.startPiece = startPiece;
+	}
+
+	public Piece getEndPiece() {
+		return endPiece;
+	}
+
+	public void setEndPiece(Piece endPiece) {
+		this.endPiece = endPiece;
+	}
+
+	public boolean isAttackWin() {
+		return attackWin;
+	}
+
+	public void setAttackWin(boolean attackWin) {
+		this.attackWin = attackWin;
+	}
+
+	public boolean isDefendWin() {
+		return defendWin;
+	}
+
+	public void setDefendWin(boolean defendWin) {
+		this.defendWin = defendWin;
+	}
+}
